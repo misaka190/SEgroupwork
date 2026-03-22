905a0da754da13aa4c0917194e0d82f22ea83327
匡奇 杨 <3997146518@qq.com>
2026-03-22 12:29:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          当前计数：{count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count + 1)}>
+          点击增加
+        </button>
       </header>
     </div>
   );
 }
 
 export default App;
+
